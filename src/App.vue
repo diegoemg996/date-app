331d048f4973a3277a1f3f556ce17f725d30574f
[Diego] <[diegoemg996@fmail.com]>
2020-06-09 15:41:30 -0500

    otro commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app" class=" contenedor">
+    <div  class="d-flex justify-content-around pb-5">
+        <router-view/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+ .titulo{
+   background: white;
+   border-radius: 10px;
+   width: 25rem ;
+   margin-left : auto;
+   margin-right : auto;
+ }
+
+ template{
+   background: black;
+ }
+ 
+
+</style>
+
---
+++ src/views/Compras.vue
@@ -0,0 +1,20 @@
+<template>
+  <div >
+
+    <Monedas></Monedas>
+
+  </div>
+</template>
+
+
+<script>
+import Monedas from '@/components/Monedas.vue'
+
+export default {
+    name: 'Compras',
+    components: {
+    Monedas
+  }
+  
+}
+</script>
