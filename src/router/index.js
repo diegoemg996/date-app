129757aed69e5d396ac4a0c366ed832d25d312f7
[Diego] <[diegoemg996@fmail.com]>
2020-06-09 13:14:03 -0500

    cambios

--- src/router/index.js
+++ src/router/index.js
@@ -17,13 +17,18 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Compras.vue')
+  },
+  {
+    path: "/index.html",
+    redirect: "/" 
   }
+  
+  
 ]
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+  
 })
 
 export default router
