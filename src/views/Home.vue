d71614b2a537841b049f48a82554fd3e8203d828
[Diego] <[diegoemg996@fmail.com]>
2020-06-09 13:46:29 -0500

    second commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home my-5">
+    
+    <MostrarUsuarios />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MostrarUsuarios from '@/components/MostrarUsuarios.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    MostrarUsuarios
+  }
+}
+</script>
